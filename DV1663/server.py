44fc87563c9d784b0666fda32db40053c995c4f4
lancelot <teddan@uniqaq.se>
2025-05-25 15:07:28 +0200

    re-added trigger and other multi table queries

--- DV1663/server.py
+++ DV1663/server.py
@@ -5,26 +5,23 @@ import sqlite3  # Database
 
 app = Flask(__name__)
 
-# Connect to SQLite
-#def get_gods():
-#    conn = sqlite3.connect('projekt_dv1663.db')
-#    cursor = conn.cursor()
-#    cursor.execute( """SELECT g.name AS God, COUNT(r.Main_character) AS Number_of_Heroes, COUNT(h.ID) AS Helps_or_Hinders
-#                        FROM gods g
-#                        LEFT JOIN relation r ON g.name = r.God
-#                        LEFT JOIN help_or_hinder h ON g.name = h.name
-#                        GROUP BY g.name
-#                        ORDER BY Number_of_Heroes DESC;""")
-#    gods = cursor.fetchall()
-#    conn.close()
-#    return gods
+def get_gods_adventures():
+    conn = sqlite3.connect('projekt_dv1663.db')
+    cursor = conn.cursor()
+    cursor.execute( """SELECT g.name AS God, COUNT(r.Main_character) AS Number_of_Heroes, COUNT(h.ID) AS Helps_or_Hinders
+                        FROM gods g
+                        LEFT JOIN relation r ON g.name = r.God
+                        LEFT JOIN help_or_hinder h ON g.name = h.name
+                        GROUP BY g.name
+                        ORDER BY Number_of_Heroes DESC;""")
+    gods = cursor.fetchall()
+    conn.close()
+    return gods
 
 def get_details(item_id):
     conn = sqlite3.connect('projekt_dv1663.db')
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-
-    # Safe query using parameter substitution to avoid SQL injection
     cursor.execute("SELECT * FROM gods WHERE name = ?", (item_id,))
     row = [tuple(elements) for elements in cursor.fetchall()]
     conn.close()
@@ -179,6 +176,10 @@ def get_gods():
 @app.route('/')
 def form():
     return render_template('form.html', gods = get_gods())
+
+@app.route('/gods_adventures')
+def gods_adventures():
+    return jsonify(get_gods_adventures())
 
 @app.route('/main_char_adventures')
 def main_char_adventures():
